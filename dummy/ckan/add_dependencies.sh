6527ce2e296598a5d1033cb25a59bbd6ca1e3291
Francesco Frassinelli <francesco.frassinelli@nina.no>
2026-04-07 15:08:02 +0200

    chg: upgrade to Python 3.11

--- dummy/ckan/add_dependencies.sh
+++ dummy/ckan/add_dependencies.sh
@@ -4,7 +4,9 @@ set -ex
 
 VERSION="2.11.4"
 
-uv add "setuptools<46" wheel Cython
+uvx crudini --set pyproject.toml project dependencies '[]'
+
+uv add "setuptools==77.0.3"
 
 uv add -r "https://raw.githubusercontent.com/ckan/ckan/refs/tags/ckan-$VERSION/requirements.txt"
 uv add "ckan @ git+https://github.com/ckan/ckan@ckan-$VERSION"
