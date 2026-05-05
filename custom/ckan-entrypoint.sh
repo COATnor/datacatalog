866f35fbe73a268579e71aadc3dadcfc2d71578d
Francesco Frassinelli <francesco.frassinelli@nina.no>
2026-05-05 14:35:12 +0200

    chore: simplify entrypoint

--- custom/ckan-entrypoint.sh
+++ custom/ckan-entrypoint.sh
@@ -1,40 +1,7 @@
 #!/bin/sh
 set -e
 
-# URL for the primary database, in the format expected by sqlalchemy (required
-# unless linked to a container called 'db')
-: ${CKAN_SQLALCHEMY_URL:=}
-# URL for solr (required unless linked to a container called 'solr')
-: ${CKAN_SOLR_URL:=}
-# URL for redis (required unless linked to a container called 'redis')
-: ${CKAN_REDIS_URL:=}
-
 CONFIG="${CKAN_CONFIG}/production.ini"
-
-abort () {
-  echo "$@" >&2
-  exit 1
-}
-
-set_environment () {
-  export CKAN_SITE_ID=${CKAN_SITE_ID}
-  export CKAN_SITE_URL=${CKAN_SITE_URL}
-  export CKAN_SQLALCHEMY_URL=${CKAN_SQLALCHEMY_URL}
-  export CKAN_SOLR_URL=${CKAN_SOLR_URL}
-  export CKAN_REDIS_URL=${CKAN_REDIS_URL}
-  export CKAN_STORAGE_PATH=/var/lib/ckan
-  export CKAN_SMTP_SERVER=${CKAN_SMTP_SERVER}
-  export CKAN_SMTP_STARTTLS=${CKAN_SMTP_STARTTLS}
-  export CKAN_SMTP_USER=${CKAN_SMTP_USER}
-  export CKAN_SMTP_PASSWORD=${CKAN_SMTP_PASSWORD}
-  export CKAN_SMTP_MAIL_FROM=${CKAN_SMTP_MAIL_FROM}
-  export CKAN_MAX_UPLOAD_SIZE_MB=${CKAN_MAX_UPLOAD_SIZE_MB}
-}
-
-write_config () {
-  echo "Generating config at ${CONFIG}..."
-  ckan generate config "$CONFIG"
-}
 
 # Wait for PostgreSQL
 while ! pg_isready -h db -U ckan; do
@@ -43,22 +10,9 @@ done
 
 # If we don't already have a config file, bootstrap
 if [ ! -e "$CONFIG" ]; then
-  write_config
+  echo "Generating config at ${CONFIG}..."
+  ckan generate config "$CONFIG"
 fi
 
-# Get or create CKAN_SQLALCHEMY_URL
-if [ -z "$CKAN_SQLALCHEMY_URL" ]; then
-  abort "ERROR: no CKAN_SQLALCHEMY_URL specified in docker-compose.yml"
-fi
-
-if [ -z "$CKAN_SOLR_URL" ]; then
-    abort "ERROR: no CKAN_SOLR_URL specified in docker-compose.yml"
-fi
-
-if [ -z "$CKAN_REDIS_URL" ]; then
-    abort "ERROR: no CKAN_REDIS_URL specified in docker-compose.yml"
-fi
-
-set_environment
 ckan --config "$CONFIG" db init
 exec "$@"
